746cc5c6566ededc6b3e16c282afeb3a63753ad7
Anderson Rodrigues <anddersonrds@gmail.com>
2021-04-09 20:49:52 -0300

    chore: add jest and testing-library

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
