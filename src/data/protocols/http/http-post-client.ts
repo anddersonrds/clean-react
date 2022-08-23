e824a6667e44eee7f6800073decb32713f240cc2
Anderson Rodrigues <anddersonrds@gmail.com>
2022-08-22 21:02:58 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-types */
 export type HttpPostParams = {
   url: string
+  body?: object
 }
 export interface HttpPostClient {
   post(params: HttpPostParams): Promise<void>
