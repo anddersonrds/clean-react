abfa90e0236759a06de9bc901b3a3b142999a544
Anderson Rodrigues <anddersonrds@gmail.com>
2022-11-14 10:36:57 -0300

    feat: ensure AxiosHttpClient calls axios with correct verb

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -4,6 +4,6 @@ import axios from 'axios'
 export class AxiosHttpClient {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async post(params: HttpPostParams<any>): Promise<void> {
-    await axios(params.url)
+    await axios.post(params.url)
   }
 }
